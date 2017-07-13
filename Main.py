4eec98fd7ab8e4867233a36784cc7d402068df2b
xkamilszczesnyx@gmail.com <W0lf3nst31n>
2017-07-13 10:26:51 +0200

    Add Label and button. After clicking the text button changes. Double-click returns to its original form.

--- Main.py
+++ Main.py
@@ -1,4 +1,21 @@
 import tkinter as tk
+from tkinter import ttk
+
+def clickMe():
+    if(action.cget('text')=="asdfasdf"):
+        action.configure(text="First")
+    else:
+        action.configure(text="asdfasdf")
+    aLabel.configure(foreground='red')
+    print(action.cget('text'))
 win = tk.Tk()
 win.title('First Program')
+aLabel= ttk.Label(win, text="First")
+aLabel.grid(column=0,row=0)
+
+action =ttk.Button(win,text="Click ME!", command = clickMe)
+action.grid(column=1,row=0)
+
+
+
 win.mainloop()
