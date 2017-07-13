6db64b37d1823a80323281a4026e78770309d5ee
xkamilszczesnyx@gmail.com <W0lf3nst31n>
2017-07-13 12:27:00 +0200

    After entering text into text box and click button this text show in Label

--- Main.py
+++ Main.py
@@ -1,13 +1,9 @@
 import tkinter as tk
+from lib2to3.fixes import fix_throw
 from tkinter import ttk
 
 def clickMe():
-    if(action.cget('text')=="asdfasdf"):
-        action.configure(text="First")
-    else:
-        action.configure(text="asdfasdf")
-    aLabel.configure(foreground='red')
-    print(action.cget('text'))
+    aLabel.configure(text=name.get())
 win = tk.Tk()
 win.title('First Program')
 aLabel= ttk.Label(win, text="First")
@@ -15,6 +11,10 @@ aLabel.grid(column=0,row=0)
 
 action =ttk.Button(win,text="Click ME!", command = clickMe)
 action.grid(column=1,row=0)
+
+name=tk.StringVar()
+xtext = ttk.Entry(win,width = 12, textvariable=name)
+xtext.grid(column=0,row=2)
 
 
 
